f459307aced1a1d60847c00e42e5e640d02cd3aa
Eyal Rozenberg <eyalr@cs.technion.ac.il>
2020-03-03 11:01:03 +0200

    learning code
    -missing wasserstein distance
    - only G1
    - parameters should be summed to 1

---
+++ spdc_helper.py
@@ -0,0 +1,230 @@
+from jax import numpy as np, jit, nn, random
+
+
+###########################################
+# Constants
+###########################################
+pi = np.pi
+c = 2.99792458e8  # the speed of light in meter/sec
+eps0 = 8.854187817e-12  # the vacuum permittivity, in Farad/meter.
+h_bar = 1.054571800e-34  # Units are m^2 kg / s, taken from http://physics.nist.gov/cgi-bin/cuu/Value?hbar|search_for=planck
+
+###########################################
+# lambda funtions:
+###########################################
+SFG_idler_wavelength    = lambda lambda_p, lambda_s: lambda_p * lambda_s / (lambda_s - lambda_p)  # Compute the idler wavelength given pump and signal
+E0                      = lambda P, n, W0: np.sqrt(P / (n * c * eps0 * np.pi * W0 ** 2))  # Calc amplitude
+Fourier                 = lambda A: (np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(A))))  # Fourier
+G1_Normalization        = lambda w: h_bar * w / (2 * eps0 * c)
+
+#################################################################
+# Create classes:
+#  - beam: all parameters of a laser beam
+#  - field: initializes E_out and E_vac
+#  - crystal: all structure arrays
+################################################################
+'''
+Class Crystal:
+----------
+initialize all strcutres of crystal
+    - dx,dy,dz : resolution in x y and z axis
+    - MaxX,MaxY : half the crystal length in x y 
+    - MaxZ: crystal length in the z axis
+    - Ref_ind : function for calcualting the refracive index of a crystal, accepts lambda in microns and temperature
+    - slab_function: function for calculating the crystal slab at each z
+    - d : half the second order nonlinear suscpetiblity
+    - poliong period (optional): the poling period of the crystal
+'''
+class Crystal:
+    def __init__(self, dx, dy, dz, MaxX, MaxY, MaxZ, Ref_ind, slab_function, d, period=0):
+        self.dz = dz  # resolution of z axis
+        self.dx = dx  # resolution of x axis
+        self.dy = dy  # resolution of y axis
+        self.MaxX = MaxX
+        self.MaxY = MaxY
+        self.MaxZ = MaxZ
+        self.x = np.arange(-MaxX, MaxX, dx)  # x axis, length 2*MaxX (transverse)
+        self.y = np.arange(-MaxY, MaxY, dy)  # y axis, length 2*MaxY  (transverse)
+        self.z = np.arange(-MaxZ / 2, MaxZ / 2, dz)  # z axis, length MaxZ (propagation)
+        self.ctype = Ref_ind  # refractive index function
+        self.slab = slab_function
+        self.d = d
+        self.poling_period = period
+
+
+
+'''
+Class BEAM:
+----------
+compute everything to do with a beam.
+    -lam - wavelength, in m
+    -crystal -  class Crystal with refractive index function
+    -T - temperature, in C
+    -waist- beam waist in meters. optional
+    -power - peak power of the beam, in W. optional
+'''
+class Beam:
+    def __init__(self, lam, crystal, T, waist=0, power=0):
+        self.lam = lam  # wavelength
+        self.waist = waist  # waist
+        self.n = crystal.ctype(lam * 1e6, T)  # refractive index
+        self.w = 2 * np.pi * c / lam  # frequency
+        self.k = 2 * np.pi * crystal.ctype(lam * 1e6, T) / lam  # wave vector
+        self.b = waist ** 2 * self.k  #
+        self.power = power  # beam power
+
+
+'''
+Class Field:
+----------
+initialize E_out and E_vac, for a given beam (class Beam) and crystal (class Crystal)
+    E_out - as in the paper, the output field, initilized as 0
+    E_vac - as in the paper, the vacuum field, initilized as gaussian noise
+    kappa - coupling constant 
+'''
+
+
+class Field:
+    def __init__(self, beam, crystal, vac_rnd):
+        Nx = len(crystal.x)
+        Ny = len(crystal.y)
+        self.E_out = np.zeros([Nx, Ny], dtype=complex)
+        vac = np.sqrt(h_bar * beam.w / (2 * eps0 * beam.n ** 2 * crystal.dx * crystal.dy * crystal.MaxZ))
+        self.E_vac = vac * (vac_rnd[0] + 1j * vac_rnd[1]) / np.sqrt(2)
+        self.kappa = 2 * 1j * beam.w ** 2 / (beam.k * c ** 2)  # we leave d_33 out and add it in the propagation function.
+        self.k = beam.k
+
+
+#######################################################
+# Functions
+######################################################
+'''
+A helper function to randomly initialize weights
+for the coefficients
+* m - number of coefficients
+* scale - all weights should sum to 'scale'
+'''
+def random_params(m, key, scale=1):
+  return scale * nn.softmax(random.normal(key, (m,)))
+
+'''
+Periodically poled crystal slab
+create the crystal slab at point z in the crystal, for poling period 2pi/delta_k
+'''
+@jit
+def PP_crystal_slab(delta_k, z):
+    return np.sign(np.cos(np.abs(delta_k) * z))
+
+
+'''
+Refractive index for MgCLN, based on Gayer et al, APB 2008
+lambda is in microns, T in celsius
+'''
+@jit
+def nz_MgCLN_Gayer(lam, T):
+    a = np.array([5.756, 0.0983, 0.2020, 189.32, 12.52, 1.32 * 10 ** (-2)])
+    b = np.array([2.860 * 10 ** (-6), 4.700 * 10 ** (-8), 6.113 * 10 ** (-8), 1.516 * 10 ** (-4)])
+    f = (T - 24.5) * (T + 570.82)
+
+    n1 = a[0]
+    n2 = b[0] * f
+    n3 = (a[1] + b[1] * f) / (lam ** 2 - (a[2] + b[2] * f) ** 2)
+    n4 = (a[3] + b[3] * f) / (lam ** 2 - (a[4]) ** 2)
+    n5 = -a[5] * lam ** 2
+
+    nz = np.sqrt(n1 + n2 + n3 + n4 + n5)
+    return nz
+
+
+'''
+Crystal propagation
+propagate through crystal using split step Fourier for 4 fields: e_out and E_vac, signal and idler.
+
+'''
+def crystal_prop(Pump, Siganl_field, Idler_field, crystal, params):
+    for z in crystal.z:
+        # pump beam:
+        E_pump = params[0]*Gaussian_beam_calc(Pump, crystal, z)  # E_pump = params[0]*Gaussian_beam_calc(Pump[0], crystal, z) + params[1]*Gaussian_beam_calc(Pump[1], crystal, z)
+        # crystal slab:
+        PP = crystal.slab(crystal.poling_period, z)
+
+        # cooupled wave equations - split step
+        # signal:
+        dEs_out_dz = Siganl_field.kappa * crystal.d * PP * E_pump * np.conj(Idler_field.E_vac)
+        dEs_vac_dz = Siganl_field.kappa * crystal.d * PP * E_pump * np.conj(Idler_field.E_out)
+
+        Siganl_field.E_out = Siganl_field.E_out + dEs_out_dz * crystal.dz
+        Siganl_field.E_vac = Siganl_field.E_vac + dEs_vac_dz * crystal.dz
+
+        # idler:
+        dEi_out_dz = Idler_field.kappa * crystal.d * PP * E_pump * np.conj(Siganl_field.E_vac)
+        dEi_vac_dz = Idler_field.kappa * crystal.d * PP * E_pump * np.conj(Siganl_field.E_out)
+
+        Idler_field.E_out = Idler_field.E_out + dEi_out_dz * crystal.dz
+        Idler_field.E_vac = Idler_field.E_vac + dEi_vac_dz * crystal.dz
+
+        # propagate
+        Siganl_field.E_out = propagate(Siganl_field.E_out, crystal.x, crystal.y, Siganl_field.k, crystal.dz) * np.exp(
+            1j * Siganl_field.k * crystal.dz)
+        Siganl_field.E_vac = propagate(Siganl_field.E_vac, crystal.x, crystal.y, Siganl_field.k, crystal.dz) * np.exp(
+            1j * Siganl_field.k * crystal.dz)
+        Idler_field.E_out = propagate(Idler_field.E_out, crystal.x, crystal.y, Idler_field.k, crystal.dz) * np.exp(
+            1j * Idler_field.k * crystal.dz)
+        Idler_field.E_vac = propagate(Idler_field.E_vac, crystal.x, crystal.y, Idler_field.k, crystal.dz) * np.exp(
+            1j * Idler_field.k * crystal.dz)
+
+    return
+
+
+'''
+Free space propagation of a Gaussian beam
+Calcualtes the 2-D distribution of a gaussian beam at point z_point
+calculation according to Boyd, second edition, 2008
+inputs:
+    - beam: class beam
+    - crystal: crystal class
+    - z_point: the z location of the beam
+Boyd 2nd eddition       
+'''
+
+def Gaussian_beam_calc(beam, crystal, z_point):
+    X, Y = np.meshgrid(crystal.x, crystal.y, indexing='ij')
+    xi = 2 * (z_point) / beam.b
+    tau = 1 / (1 + 1j * xi)
+    E = E0(beam.power, beam.n, beam.waist) * tau * np.exp(-((X) ** 2 + (Y) ** 2) / (beam.waist ** 2) * tau) * np.exp(
+        1j * beam.k * (z_point))
+    return E
+
+
+'''
+Free Space propagation using the free space transfer function 
+(two  dimensional), according to Saleh
+ Function reciveing:        
+        -Beam: class beam
+        -x,y : spatial vectors
+       -d: The distance to propagate
+The output is the propagated field.
+Using CGS, or MKS, Boyd 2nd eddition       
+'''
+
+@jit
+def propagate(A, x, y, k, dz):
+    dx = np.abs(x[1] - x[0])
+    dy = np.abs(y[1] - y[0])
+    # define the fourier vectors
+    X, Y = np.meshgrid(x, y, indexing='ij')
+    KX = 2 * np.pi * (X / dx) / (np.size(X, 1) * dx)
+    KY = 2 * np.pi * (Y / dy) / (np.size(Y, 1) * dy)
+    # The Free space transfer function of propagation, using the Fresnel approximation
+    # (from "Engineering optics with matlab"/ing-ChungPoon&TaegeunKim):
+    H_w = np.exp(-1j * dz * (np.square(KX) + np.square(KY)) / (2 * k))
+    # (inverse fast Fourier transform shift). For matrices, ifftshift(X) swaps the
+    # first quadrant with the third and the second quadrant with the fourth.
+    H_w = np.fft.ifftshift(H_w)
+    # Fourier Transform: move to k-space
+    G = np.fft.fft2(A)  # The two-dimensional discrete Fourier transform (DFT) of A.
+    # propoagte in the fourier space
+    F = np.multiply(G, H_w)
+    # inverse Fourier Transform: go back to real space
+    Eout = np.fft.ifft2(F)  # [in real space]. E1 is the two-dimensional INVERSE discrete Fourier transform (DFT) of F1
+    return Eout
---
+++ spdc_inv.py
@@ -0,0 +1,190 @@
+from __future__ import print_function, division, absolute_import
+from jax import grad, vmap, ops
+from jax.numpy import kron, linalg as la
+
+from spdc_helper import *
+import matplotlib.pyplot as plt
+
+import os, time
+os.environ["CUDA_VISIBLE_DEVICES"] = '6'
+
+start_time_initialization = time.time()
+
+# are we in learning mode or infering from simulation
+learn_mode  = False
+
+###############################
+# Target function and results #
+###############################
+show_res      = True  # display and show results
+# saving results
+save_res      = False
+res_path      = 'results/'          # path to folder. should be given as a user-parameter
+res_name      = 'G1_ss'
+
+# saving as target
+save_tgt      = False
+Gt_path       = 'targets/'          # path o folder. should be given as a user-parameter
+G1ss_t_name   = 'G1_ss_t'
+
+# load target G1, G2
+if learn_mode:
+    G1_ss_t     = np.load(Gt_path+G1ss_t_name+'.npy')  # target signal probability-density
+    G2t         = None
+
+
+n_coeff     = 1  # coefficients of beam-basis functions
+param_scale = 1
+step_size   = 0.0001
+num_epochs  = 5
+batch_size  = 50   # 20, 20, 50, 100
+N           = 100  # 100, 500, 1000  number of iterations / dataset size
+
+num_batches = N/batch_size
+assert N % batch_size == 0, "num_batches should be 'signed integer'"
+
+
+###########################################
+# Structure arrays
+###########################################
+# initialize crystal and structure arrays
+d33         = 23.4e-12  # in meter/Volt.[LiNbO3]
+PP_SLT      = Crystal(10e-6, 10e-6, 1e-6, 200e-6, 200e-6, 5e-3, nz_MgCLN_Gayer, PP_crystal_slab, d33)
+R           = 0.1  # distance to far-field screenin meters
+Temperature = 50
+M           = len(PP_SLT.x)  # simulation size
+
+###########################################
+# Interacting wavelengths
+##########################################
+# Initiialize the interacting beams
+
+# * define two pump's function (for now n_coeff must be 2) to define the pump *
+# * this should be later changed to the definition given by Sivan *
+Pump    = Beam(532e-9, PP_SLT, Temperature, 100e-6, 0.03)  # wavelength, crystal, tmperature,waist,power
+Signal  = Beam(1064e-9, PP_SLT, Temperature)
+Idler   = Beam(SFG_idler_wavelength(Pump.lam, Signal.lam), PP_SLT, Temperature)
+# phase mismatch
+delta_k = Pump.k - Signal.k - Idler.k
+PP_SLT.poling_period = 1.003 * delta_k
+
+
+
+###########################################
+# Set dataset
+##########################################
+# Build a dataset of pairs Ai_vac, As_vac
+# seed vacuum samples
+key = random.PRNGKey(1986)
+
+# seed shuffle batches in epochs
+key_batch_epoch = random.split(random.PRNGKey(1989), num_epochs)
+
+Nx = len(PP_SLT.x)
+Ny = len(PP_SLT.y)
+
+vac_rnd = random.normal(key, (N, 2, 2, Nx, Ny))
+# N iteration, 2-for vac states for signal and idler, 2 - real and imag, Nx X Ny for beam size)
+
+# Build Diagonal-Element indicator matrix for the Kronicker products
+Kron_diag = np.zeros((M ** 2, M ** 2))
+Kron_diag = ops.index_update(Kron_diag, ops.index[::M+1,::M+1], 1)
+
+params = random_params(n_coeff, random.PRNGKey(0))
+print("--- the parameters initiated are: {} ---".format(params))
+print("--- initialization time: %s seconds ---" % (time.time() - start_time_initialization))
+start_time = time.time()
+
+# forward model
+def predict(params, vac_): # vac_ = vac_s, vac_i
+    # initialize the vacuum and output fields:
+    Siganl_field    = Field(Signal, PP_SLT, vac_[0])
+    Idler_field     = Field(Idler, PP_SLT, vac_[1])
+    # Propagate through the crystal:
+    crystal_prop(Pump, Siganl_field, Idler_field, PP_SLT, params)
+    # Coumpute k-space far field using FFT:
+    # normalization factors
+    FarFieldNorm_signal = (2 * PP_SLT.MaxX) ** 2 / (np.size(Siganl_field.E_out) * Signal.lam * R)
+    # FFT:
+    E_s_out_k   = FarFieldNorm_signal * Fourier(Siganl_field.E_out)
+    G1_ss       = kron(np.conj(E_s_out_k), E_s_out_k) / N
+    return G1_ss
+
+# Make a batched version of the `predict` function
+batched_predict = vmap(predict, in_axes=(None, 0))
+# batched_preds   = batched_predict(params, vac_rnd)
+# G1_ss           = batched_preds.sum(0)
+# P_ss            = np.reshape(G1_ss[Kron_diag != 0], (M, M)) / G1_Normalization(Signal.w)
+
+
+def loss(params, vac_, G1_ss_t, G2t): # vac_ = vac_s, vac_i, G2t = G1/2 target corrletion matrices
+    batched_preds   = batched_predict(params, vac_)
+    G1_ss            = batched_preds.sum(0)
+    return la.norm(G1_ss - G1_ss_t)  # should be replaced to wasserstein distance
+
+@jit
+def update(params, x, y1, y2=None):
+  grads = grad(loss)(params, x, y1, y2)
+  return [(w - step_size * dw)
+          for (w), (dw) in zip(params, grads)]
+
+
+def get_train_batches(vac_, key_):
+    vac_shuff = random.shuffle(key_, vac_, axis=0)
+    batch_arr = np.split(vac_shuff, num_batches, axis=0)
+    return batch_arr
+
+if learn_mode:
+    for epoch in range(num_epochs):
+      start_time_epoch = time.time()
+      batch_set = get_train_batches(vac_rnd, key_batch_epoch[epoch])
+      for x in batch_set:
+        params = update(params, x, G1_ss_t)
+      epoch_time = time.time() - start_time_epoch
+      print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
+      print("--- the parameters optimized are: {} ---".format(params))
+
+
+################
+# Plot G1(k,k) #
+################
+
+# show last epoch result
+batched_preds   = batched_predict(params, vac_rnd)
+G1_ss           = batched_preds.sum(0)
+
+if save_res:
+    np.save(res_path+res_name, G1_ss)
+
+if save_tgt:
+    np.save(Gt_path+Gt_path, G1_ss)
+
+if show_res:
+    P_ss = np.reshape(G1_ss[Kron_diag != 0], (M, M)) / G1_Normalization(Signal.w)
+
+    FF_position_axis = lambda dx, MaxX, k, R: np.arange(-1 / dx, 1 / dx, 1 / MaxX) * (np.pi * R / k)
+    FFcoordinate_axis_Idler = 1e3 * FF_position_axis(PP_SLT.dx, PP_SLT.MaxX, Idler.k / Idler.n, R)
+    FFcoordinate_axis_Signal = 1e3 * FF_position_axis(PP_SLT.dx, PP_SLT.MaxX, Signal.k / Signal.n, R)
+
+    # AK, NOV24: I added a far-field position axis extents, in mm.
+    extents_FFcoordinates_signal = [min(FFcoordinate_axis_Signal), max(FFcoordinate_axis_Signal),
+                                    min(FFcoordinate_axis_Signal), max(FFcoordinate_axis_Signal)]
+    extents_FFcoordinates_idler = [min(FFcoordinate_axis_Idler), max(FFcoordinate_axis_Idler), min(FFcoordinate_axis_Idler),
+                                   max(FFcoordinate_axis_Idler)]
+
+    # calculate theoretical angle for signal
+    theoretical_angle = np.arccos((Pump.k - PP_SLT.poling_period) / 2 / Signal.k)
+    theoretical_angle = np.arcsin(Signal.n * np.sin(theoretical_angle) / 1)  # Snell's law
+
+    plt.figure()
+    plt.imshow(np.real(P_ss * 1e-6), extent=extents_FFcoordinates_signal)  # AK, Dec08: Units of counts/mm^2*sec
+    plt.plot(1e3 * R * np.tan(theoretical_angle), 0, 'xw')
+    plt.xlabel(' x [mm]')
+    plt.ylabel(' y [mm]')
+    plt.title('Single photo-detection probability, Far field')
+    plt.colorbar()
+    plt.show()
+
+print("--- the parameters optimized are: {} ---".format(params))
+print("--- running time: %s seconds ---" % (time.time() - start_time))
+exit()
